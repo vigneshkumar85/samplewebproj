d2803c508fe15307df1a4d9532c6cd46c11f4b99
Vignesh Kumar <vigneshkumar85@gmail.com>
2021-08-18 15:05:43 -0700

    Initial_Commit

--- sampleweb/src/main/java/com/example/springboot/HelloController.java
+++ sampleweb/src/main/java/com/example/springboot/HelloController.java
@@ -8,7 +8,7 @@ public class HelloController {
 
 	@GetMapping("/")
 	public String index() {
-		return "Greetings from Spring Boot Vignesh test4!";
+		return "Greetings from Spring Boot test4!";
 	}
 
 }
